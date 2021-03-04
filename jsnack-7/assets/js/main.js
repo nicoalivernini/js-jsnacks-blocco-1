ed659d1d5b7f924042c3d9c538a71cafa2d2b711
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-04 13:19:15 +0100

    Esercizio completato

--- jsnack-7/assets/js/main.js
+++ jsnack-7/assets/js/main.js
@@ -5,14 +5,8 @@ var potenza = 2;
 var numeriPotenza = [];
 
 //Calcolo potenza
-<<<<<<< HEAD
 for (var i = 2; i < 11; i++) {
   numeriPotenza.push(potenza**i)
-  
-=======
-for (var i = 0; Math.pow(2, i) < 1000; i++) {
-  numeriPotenza.push(Math.pow(2, i))
->>>>>>> parent of e063515... Aggiornamento jsnack 7
   //Stampo potenza
   console.log(numeriPotenza);
 }
